d7414ac0c9f17156e78422a4bfb926cb9267a644
ybettadji <younes.bettadji@gmail.com>
2022-12-20 13:45:40 +0100

    Ticket 5 - search a product by name

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,7 +1,8 @@
 import { Controller, Get, Param } from '@nestjs/common';
-import { ApiOkResponse, ApiOperation } from '@nestjs/swagger';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { ProductService } from './product.service';
 
+@ApiTags('Product Module')
 @Controller('product')
 export class ProductController {
 
@@ -12,5 +13,12 @@ export class ProductController {
     @ApiOkResponse({ description: "The product is returned. If there is no product is attached to the code entered, an empty object is returned" })
     getByCode(@Param('code') code: string): Promise<object> {
         return this.productService.getByCode(code)
+    }
+
+    @Get('getByName/:product_name')
+    @ApiOperation({ summary: "Search in the database a product with its name" })
+    @ApiOkResponse({ description: "The product is returned. If there is no product is attached to the name entered, an empty object is returned" })
+    getByName(@Param('product_name') product_name: string): Promise<object> {
+        return this.productService.getByName(product_name)
     }
 }
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -9,7 +9,22 @@ export class ProductService {
     constructor(@InjectModel('Product') private productModel: Model<Product>) { }
 
     async getByCode(code: string): Promise<object> {
-        const product = await this.productModel.find({ "code": code })
-        return product
+
+        try {
+            const product = await this.productModel.find({ "code": code })
+            return product
+        } catch (error) {
+            throw new Error("Error while searching for the product in the database");
+        }
+    }
+
+    async getByName(product_name: string): Promise<object> {
+
+        try {
+            const product = await this.productModel.find({ "product_name": product_name })
+            return product
+        } catch (error) {
+            throw new Error("Error while searching for the product in the database");
+        }
     }
 }
