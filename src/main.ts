79e27935e7df51c361bda26e8fe2c97ccab8d270
ybettadji <younes.bettadji@gmail.com>
2022-12-20 13:38:59 +0100

    Ticket 4 - Swagger init

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Documentation Test Technique Noticia x Younes')
+    .setDescription("This API allows you to search for a product in the database based on its code or name.")
+    .setVersion('1.0')
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(`${process.env.APP_PORT}`);
 }
 bootstrap();
--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { ApiOkResponse, ApiOperation } from '@nestjs/swagger';
 import { ProductService } from './product.service';
 
 @Controller('product')
@@ -7,6 +8,8 @@ export class ProductController {
     constructor(private productService: ProductService) { }
 
     @Get('getByCode/:code')
+    @ApiOperation({ summary: "Search in the database a product with its code" })
+    @ApiOkResponse({ description: "The product is returned. If there is no product is attached to the code entered, an empty object is returned" })
     getByCode(@Param('code') code: string): Promise<object> {
         return this.productService.getByCode(code)
     }
